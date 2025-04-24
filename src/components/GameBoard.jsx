77ebbb3642c926e291021ae742271e518bfb5be9
miriyan19425 <miriyan19425@gmail.com>
2025-04-25 00:14:01 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,13 @@
-function GameBoard(){
-    return <section></section>;
+import Info from './Info';
+
+function GameBoard({playerName}){
+    return (
+        <section>
+            <Info info={playerName} />
+            <Info info={0} />
+            <Info info={0} />
+        </section>
+    );
 }
 
 export default GameBoard;
