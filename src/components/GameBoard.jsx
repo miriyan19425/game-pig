2fc11e0c128bf0337796cb88fc6b73755cd79914
miriyan19425 <miriyan19425@gmail.com>
2025-04-26 02:19:12 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,11 +1,16 @@
 import Info from './Info';
+import DieImg from './DieImg';
+import Button from './Button';
 
 function GameBoard({playerName}){
     return (
         <section>
-            <Info info={playerName} />
-            <Info info={0} />
-            <Info info={0} />
+            <Info clN='name'>{playerName}</Info>
+            <DieImg />
+            <Info clN='zero'>0</Info>
+            <Button>Roll</Button>
+            <Info clN='savedPoints'>0</Info>
+            <Button>Save</Button>
         </section>
     );
 }
