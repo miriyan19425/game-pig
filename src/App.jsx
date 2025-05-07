3568f63a56956a21dc3f99753400c3d44313743c
miriyan19425 <miriyan19425@gmail.com>
2025-05-08 01:15:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import GameBoard from './components/GameBoard';
 function App() {
    
   return  (
-    <main>
+    <>
       <GameBoard playerName='PLAYER 1' />
       <GameBoard playerName='PLAYER 2' />
-    </main>
+    </>
   ); 
 }
 
