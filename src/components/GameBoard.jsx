441715dd10d91239bee938e4ab8fe469632c1df8
miriyan19425 <miriyan19425@gmail.com>
2025-05-09 09:17:18 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,17 @@
-import {useState} from 'react';
 import Info from './Info';
 import DieImg from './DieImg';
 import Button from './Button';
 
-function GameBoard({playerName}){
+function GameBoard({playerName, points, unsavedPoints, savedPoints, handleSavedPoints, handleUnsavedPoints}){
     
-    const [score, setScore] = useState(1);
-    const [unsavedScore, setUnsavedScore] = useState(0);
-    const [savedScore, setSavedScore] = useState(0);
-
-    function roll(){
-        const randomInt = Math.trunc(Math.random() * 6) + 1;
-        setScore(randomInt);
-        randomInt === 1 ? setUnsavedScore(0) : setUnsavedScore(prev => prev += randomInt);
-    }
-
-    function save(){
-        setSavedScore(prev => prev += unsavedScore);
-        setUnsavedScore(0);
-    }
-
     return (
         <section>
             <Info clN='name'>{playerName}</Info>
-            <DieImg diePoints={score} />
-            <Info clN={unsavedScore === 0 ? 'zero' : 'nonzero'}>{unsavedScore}</Info>
-            <Button handleClick={roll}>Roll</Button>
-            <Info clN='saved'>{savedScore}</Info>
-            <Button handleClick={save}>Save</Button>
+            <DieImg diePoints={points} />
+            <Info clN={unsavedPoints === 0 ? 'zero' : 'nonzero'}>{unsavedPoints}</Info>
+            <Button handleClick={handleUnsavedPoints}>Roll</Button>
+            <Info clN='saved'>{savedPoints}</Info>
+            <Button handleClick={handleSavedPoints}>Save</Button>
         </section>
     );
 }
