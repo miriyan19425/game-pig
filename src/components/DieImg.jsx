24ebeb211e251417040e6b116f6c6ca2747279af
miriyan19425 <miriyan19425@gmail.com>
2025-06-14 20:47:14 +0300

    Update DieImg.jsx

--- src/components/DieImg.jsx
+++ src/components/DieImg.jsx
@@ -4,12 +4,13 @@ import Three from '../assets/dice-3.png';
 import Four from '../assets/dice-4.png';
 import Five from '../assets/dice-5.png';
 import Six from '../assets/dice-6.png';
+import styles from './DieImg.module.css';
 
 const dieImages = [One, Two, Three, Four, Five, Six];
 
 function DieImg({ diePoints }) {
   if (diePoints < 1 || diePoints > 6) return null; // Handle invalid diePoints
-  return <img src={dieImages[diePoints - 1]} alt={`Die showing ${diePoints}`} className='die-image'/>;
+  return <img src={dieImages[diePoints - 1]} alt={`Die showing ${diePoints}`} className={styles.dieImage}/>;
 }
 
 export default DieImg;
