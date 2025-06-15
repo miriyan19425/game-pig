1c3aa5a964eabfe5772b6fd8e756d6fdafffd193
miriyan19425 <miriyan19425@gmail.com>
2025-06-15 10:42:31 +0300

    Update WinnerModal.jsx

--- src/components/WinnerModal.jsx
+++ src/components/WinnerModal.jsx
@@ -4,7 +4,7 @@ import styles from './WinnerModal.module.css';
 function WinnerModal({ name, handleNewGame }) {
   return (
     <dialog className={styles.modal} open>
-      <h1 className={styles.win-message}>{name} wins!</h1>
+      <h1 className={styles.winMessage}>{name} wins!</h1>
       <form method="dialog">
         <Button handleClick={handleNewGame}>New Game</Button>
       </form>
