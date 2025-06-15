44e7f8da5afbf12823be31c9800bca4874625e8c
miriyan19425 <miriyan19425@gmail.com>
2025-06-15 10:41:30 +0300

    Update GameBoard.jsx

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,17 @@
 import Info from './Info';
 import DieImg from './DieImg';
 import Button from './Button';
+import styles from './GameBoard.module.css';
 
 function GameBoard({playerName, points, unsavedPoints, savedPoints, handleSavedPoints, handleUnsavedPoints}){
     
     return (
-        <section className='board'>
-            <Info clN='name'>{playerName}</Info>
+        <section className={styles.board}>
+            <Info clN={styles.name}>{playerName}</Info>
             <DieImg diePoints={points} />
-            <Info clN={unsavedPoints === 0 ? 'zero' : 'nonzero'}>{unsavedPoints}</Info>
+            <Info clN={`${unsavedPoints === 0 ? styles.zero : styles.nonzero}`}>{unsavedPoints}</Info>
             <Button handleClick={handleUnsavedPoints}>Roll</Button>
-            <Info clN='saved'>{savedPoints}</Info>
+            <Info clN={styles.saved}>{savedPoints}</Info>
             <Button handleClick={handleSavedPoints}>Save</Button>
         </section>
     );
